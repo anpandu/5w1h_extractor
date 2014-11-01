b8aceb3c1c0e814df3f4430fd37dca347223bff6
anpandu <anpandumail@gmail.com>
2014-11-01 21:33:10 +0700

    tokenize function v1

--- module/tokenizer.py
+++ module/tokenizer.py
@@ -1,93 +1,68 @@
-# from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize
 import re
 
 
 class Tokenizer(object):
 
 	def __init__(self):
-		self.stopwords = ['dan', 'atau', 'tetapi', 'tapi', 'akan tetapi', 'jika', 'kalau', 'karena', 'walau', 'walaupun', 'juga', 'jadi', 'maka', 'sehingga', 'supaya', 'agar', 'hanya', 'lagi', 'lagipula', 'lalu', 'sambil', 'melainkan', 'namun', 'padahal', 'sedangkan', 'demi', 'untuk', 'apabila', 'sebab', 'asalkan', 'meskipun', 'biarpun', 'biar', 'seperti', 'daripada', 'bahkan', 'apalagi', 'yakni', 'adalah', 'yaitu', 'ialah', 'bahwa', 'kecuali', 'selain', 'misalnya', 'itu', 'di', 'yang', 'dengan', 'ke', 'anda', 'sangat', 'ini', 'dari']
+		self.entityjoin = ['dan', 'untuk']
+		self.entityjoinexception = ['Rp']
 
 	def tokenize(self, str):
-		# is string
-		res = str
-		res = self.removeNonAscii(res)
-		res = self.toLowercase(res)
-		res = self.spaceCorrect(res)
-		res = word_tokenize(res) # !!! :
-		# is list
-		res = self.removeSymbol(res)
-		res = self.stripSymbol(res)
-		res = self.removeDigit(res)
-		res = self.removeStopWords(res, self.stopwords)
+		res = word_tokenize(str) # !!! :
 		return res
 
-	def tokenizeWithStopwords(self, str):
-		# is string
-		res = str
-		res = self.removeNonAscii(res)
-		res = self.toLowercase(res)
-		res = self.spaceCorrect(res)
-		res = word_tokenize(res) # !!! :
-		# is list
-		res = self.removeSymbol(res)
-		res = self.stripSymbol(res)
-		res = self.removeDigit(res)
-		return res
+	def tokenize2(self, str): # !!! kata 'dan' pas terakhir
+		rawtokens = str.split(' ')
+		
+		# split non-alnum first char of token
+		again = True
+		while again:
+			rawtokens2 = []
+			for i, rawtoken in enumerate(rawtokens):
+				if (not rawtoken[0].isalnum() and len(rawtoken)>1):
+					rawtokens2.append(rawtoken[0])
+					rawtokens2.append(rawtoken[1:])
+				else: 
+					rawtokens2.append(rawtoken)
+			rawtokens = rawtokens2
+			again = False
+			for i, rawtoken in enumerate(rawtokens):
+				again = again or (not rawtoken[0].isalnum() and len(rawtoken)>1)
+		
+		# split non-alnum last char of token
+		again = True
+		while again:
+			rawtokens2 = []
+			for i, rawtoken in enumerate(rawtokens):
+				if (not rawtoken[-1].isalnum() and len(rawtoken)>1):
+					rawtokens2.append(rawtoken[:-1])
+					rawtokens2.append(rawtoken[-1])
+				else: 
+					rawtokens2.append(rawtoken)
+			rawtokens = rawtokens2
+			again = False
+			for i, rawtoken in enumerate(rawtokens):
+				again = again or (not rawtoken[-1].isalnum() and len(rawtoken)>1)
+
+		# join uppercased words to one word/token
+		tokens = []
+		temp = []
+		for rawtoken in rawtokens:
+			if ((rawtoken[0].isupper() or (rawtoken in self.entityjoin)) and (rawtoken not in self.entityjoinexception)):
+				temp.append(rawtoken)
+			elif (len(temp)>0):
+				tokens.append(' '.join(temp))
+				temp = []
+				tokens.append(rawtoken)
+			else: 
+				tokens.append(rawtoken)
+		return tokens
 
 	def getSentenceFromText(self, str):
 		res = str.split('. ')
 		res = [item.split(' - ') for item in res]
 		res = [item for sublist in res for item in sublist]
-		return res
-
-	def removeStopWords(self, words, stopwords):
-		res = [word for word in words if word not in stopwords]
-		return res
-
-	def toLowercase(self, str):
-		return str.lower()
-
-	def spaceCorrect(self, str):
-		return str.replace('jt', ' jt').replace('m2', ' m2').replace('juta', ' juta').replace('rp', ' rp ').replace('0m', '0 m').replace('0rb', ' rb ')
-
-	def removeDigit(self, arr):
-		res = arr
-		removed_chars = [".", ",", "-", "x", "+", "/", "lt", "lb", "km", "kt", ":"]
-		for rc in removed_chars:
-			res = [item for item in res if (self._removeDigit(item).isdigit()==0)]
-		return res
-
-	def _removeDigit(self, str):
-		res = str
-		removed_chars = [".", ",", "-", "x", "+", "/", "lt", "lb", "km", "kt", ":"]
-		for rc in removed_chars:
-			res = res.replace(rc, "")
-		return res
-
-	def removeSymbol(self, arr):
-		res = arr
-		removed_chars = [
-			"(", ")", "<", ">", 
-			"/", "\\", 
-			".", ",", ";", "|", "?", 
-			":", "=", 
-			"!", "@", "#", "$", "%", "&", "*", "-", "+"
-		]
-		for rc in removed_chars:
-			res = [item for item in res if item != rc]
-		return res
-
-	def stripSymbol(self, arr):
-		res = arr
-		removed_chars = [
-			"(", ")", "<", ">", 
-			"/", "\\", 
-			".", ",", ";", "|", "?", 
-			":", "=", 
-			"!", "@", "#", "$", "%", "&", "*", "-", "+"
-		]
-		for rc in removed_chars:
-			res = [item.strip(rc) for item in res]
 		return res
 
 	def removeNonAscii(self, str):
