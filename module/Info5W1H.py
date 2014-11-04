e8b46bd7b9ba1d0e12f27a865cc285909da7c2bb
anpandu <anpandumail@gmail.com>
2014-11-05 00:14:04 +0700

    Info5W1H class

---
+++ module/Info5W1H.py
@@ -0,0 +1,10 @@
+class Info5W1H(object):
+
+	def __init__(self, _what, _who, _when, _where, _why, _how, _text):
+		self.what = _what
+		self.who = _who
+		self.when = _when
+		self.where = _where
+		self.why = _why
+		self.how = _how
+		self.text = _text
--- module/dataprovider.py
+++ module/dataprovider.py
@@ -1,4 +1,5 @@
 import MySQLdb
+from Info5W1H import Info5W1H
 
 
 class MDP(object):
@@ -27,22 +28,8 @@ class MDP(object):
 			self.conn.rollback()
 
 	def get5w1h(self, limit=0):
-		if (limit==0):
-			return self.getQuery("SELECT 5w1hs.what, 5w1hs.who, 5w1hs.when, 5w1hs.where, 5w1hs.why, 5w1hs.how, articles.text, articles.title FROM 5w1hs, articles WHERE 5w1hs.article_id = articles.id AND user_id=6")
-
-
-
-class TextDP(object):
-
-	def __init__(self):
-		pass
-
-	# def getIndoWordList(self):
-	# 	arr = open("indowordlists/05-ivanlanin2011-sort-alpha.lst").read().split("\n")
-	# 	return arr[:len(arr)-1]
-
-	# def getPropDescCWWordList(self):
-	# 	arr = open("indowordlists/propdesc_cw.lst").read().split("\n")
-	# 	return arr[:len(arr)-1]
+		que_res = self.getQuery("SELECT 5w1hs.what, 5w1hs.who, 5w1hs.when, 5w1hs.where, 5w1hs.why, 5w1hs.how, articles.text, articles.title FROM 5w1hs, articles WHERE 5w1hs.article_id = articles.id AND user_id=6")
+		res = [Info5W1H(item[0], item[1], item[2], item[3], item[4], item[5], item[6]) for item in que_res]
+		return res
 
 
