ab8708d436c761ce263190d32dfc25f7ea8e6756
anpandu <anpandumail@gmail.com>
2014-11-07 23:58:56 +0700

    bugfix getSentence, null pointer

--- module/tokenizer.py
+++ module/tokenizer.py
@@ -58,6 +58,7 @@ class Tokenizer(object):
 		res = str.split('. ')
 		res = [item.split(' - ') for item in res]
 		res = [item for sublist in res for item in sublist]
+		res = [item for item in res if item is not ''] #remove null
 		# remove dot in last char of last sentence
 		last_sentence = res[len(res)-1]
 		res[len(res)-1] = last_sentence[:-1] if (last_sentence[-1]=='.') else last_sentence
