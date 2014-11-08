63b4aee82aa479ed528272800c18cc929cd0e8d2
anpandu <anpandumail@gmail.com>
2014-11-08 23:51:32 +0700

    Feature Extractor: prevToken & nextToken

--- module/featureextractor.py
+++ module/featureextractor.py
@@ -12,3 +12,21 @@ class FeatureExtractor(object):
 		for doc_token in doc_tokens:
 			res += 1 if (doc_token==word) else 0
 		return res
+
+	def prevToken(self, word, doc):
+		doc_tokens = self.tokenizer.getTokens(doc)
+		res = []
+		for i, doc_token in enumerate(doc_tokens):
+			if (doc_token==word and i>0):
+				res.append(doc_tokens[i-1])
+		return res
+
+	def nextToken(self, word, doc):
+		doc_tokens = self.tokenizer.getTokens(doc)
+		limit = len(doc_tokens)
+		res = []
+		for i, doc_token in enumerate(doc_tokens):
+			if (doc_token==word and i<limit-1):
+				res.append(doc_tokens[i+1])
+		return res
+
