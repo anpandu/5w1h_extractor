4bb1efa54c5483f888a2bda38882fa3ab89ffb44
anpandu <anpandumail@gmail.com>
2014-11-19 02:22:36 +0700

    TestTokenizer: test cases

--- tests/TestTokenizer.py
+++ tests/TestTokenizer.py
@@ -1,3 +1,29 @@
+from module.tokenizer import Tokenizer 
+
 class TestTokenizer:
-    def test_c(self):
-        assert 'c' == 'c'
+
+    @classmethod
+    def setup_class(self):
+        self.paragraph1 = 'Forum Indonesia untuk Transparansi Anggaran (Fitra) telah menduga PT Ghalia Indonesia Printing tak akan berhasil menyelesaikan tender naskah ujian nasional. Koordinator Investigasi dan Advokasi Uchok Sky Khadafi menilai proses tender perusahaaan tersebut ganjil. Ghalia menawarkan harga yang lebih tinggi, Rp 22,8 miliar. Namun kementerian Pendidikan dan Kebudayaan tetap memenangkan perusahaan tersebut. PT Ghalia Indonesia Printing adalah perusahaan yang mencetak naskah. Provinsi tersebut yakni Kalimantan Selatan, Kalimantan Timur, Sulawesi Utara, Sulawesi Tengah, Sulawesi Selatan, Sulawesi Tenggara, Bali, Nusa Tenggara Barat, Nusa Tenggara Timur, Gorontalo, dan Sulawesi Barat. Direktur Ghalia, Hamzah Lukman.'
+
+    def test_getTokens(self):
+    	sentence1 = "hello world"
+    	sentence2 = "hello world "
+    	sentence3 = "Hello world, my name is Alice (123)."
+        assert Tokenizer.getTokens(sentence1) == ['hello', 'world']
+        assert Tokenizer.getTokens(sentence2) == ['hello', 'world']
+        assert Tokenizer.getTokens(sentence3) == ['Hello', 'world', ',', 'my', 'name', 'is', 'Alice', '(', '123', ')', '.']
+
+    def test_getTerms(self):
+    	pterms = ['Forum Indonesia', 'untuk', 'Transparansi Anggaran', '(', 'Fitra', ')', 'telah', 'menduga', 'PT Ghalia Indonesia Printing', 'tak', 'akan', 'berhasil', 'menyelesaikan', 'tender', 'naskah', 'ujian', 'nasional', '.', 'Koordinator Investigasi', 'dan', 'Advokasi Uchok Sky Khadafi', 'menilai', 'proses', 'tender', 'perusahaaan', 'tersebut', 'ganjil', '.', 'Ghalia', 'menawarkan', 'harga', 'yang', 'lebih', 'tinggi', ',', 'Rp', '22,8', 'miliar', '.', 'Namun', 'kementerian', 'Pendidikan', 'dan', 'Kebudayaan', 'tetap', 'memenangkan', 'perusahaan', 'tersebut', '.', 'PT Ghalia Indonesia Printing', 'adalah', 'perusahaan', 'yang', 'mencetak', 'naskah', '.', 'Provinsi', 'tersebut', 'yakni', 'Kalimantan Selatan', ',', 'Kalimantan Timur', ',', 'Sulawesi Utara', ',', 'Sulawesi Tengah', ',', 'Sulawesi Selatan', ',', 'Sulawesi Tenggara', ',', 'Bali', ',', 'Nusa Tenggara Barat', ',', 'Nusa Tenggara Timur', ',', 'Gorontalo', ',', 'dan', 'Sulawesi Barat', '.', 'Direktur Ghalia', ',', 'Hamzah Lukman', '.']
+        assert Tokenizer.getTerms(self.paragraph1) == pterms
+
+
+    def test_getSentences(self):
+    	psentences = ['Forum Indonesia untuk Transparansi Anggaran (Fitra) telah menduga PT Ghalia Indonesia Printing tak akan berhasil menyelesaikan tender naskah ujian nasional', 'Koordinator Investigasi dan Advokasi Uchok Sky Khadafi menilai proses tender perusahaaan tersebut ganjil', 'Ghalia menawarkan harga yang lebih tinggi, Rp 22,8 miliar', 'Namun kementerian Pendidikan dan Kebudayaan tetap memenangkan perusahaan tersebut', 'PT Ghalia Indonesia Printing adalah perusahaan yang mencetak naskah', 'Provinsi tersebut yakni Kalimantan Selatan, Kalimantan Timur, Sulawesi Utara, Sulawesi Tengah, Sulawesi Selatan, Sulawesi Tenggara, Bali, Nusa Tenggara Barat, Nusa Tenggara Timur, Gorontalo, dan Sulawesi Barat', 'Direktur Ghalia, Hamzah Lukman']
+        assert Tokenizer.getSentences(self.paragraph1) == psentences
+
+
+    def test_removeNonAscii(self):
+    	sentence1 = u"\u0411hai"
+        assert Tokenizer.removeNonAscii(sentence1) == "hai"
