92fc08843d860fc47f193335203b610c684f51f3
anpandu <anpandumail@gmail.com>
2014-11-20 14:00:55 +0700

    TestInfo5W1H: bugfix typo

--- tests/TestInfo5W1H.py
+++ tests/TestInfo5W1H.py
@@ -13,6 +13,7 @@ class TestInfo5W1H:
         self.how = ""
 
     def test_init(self):
-    	assert info.text == "Mama, just killed a man, Put a gun against his head, Pulled my trigger, now he's dead. Mama, life had just begun, but now I've gone and thrown it all away."
+        info = Info5W1H(self.what, self.who, self.when, self.where, self.why, self.how, self.text)
+        assert info.text == "Mama, just killed a man, Put a gun against his head, Pulled my trigger, now he's dead. Mama, life had just begun, but now I've gone and thrown it all away."
     	assert info.sentences == ["Mama, just killed a man, Put a gun against his head, Pulled my trigger, now he's dead", "Mama, life had just begun, but now I've gone and thrown it all away"]
     	assert info.tokenized_sentences == [["Mama", ",", "just", "killed", "a", "man", ",", "Put", "a", "gun", "against", "his", "head", ",", "Pulled", "my", "trigger", ",", "now", "he's", "dead"], ["Mama", ",", "life", "had", "just", "begun", ",", "but", "now", "I've", "gone", "and", "thrown", "it", "all", "away"]]
