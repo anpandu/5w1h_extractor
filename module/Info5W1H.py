ecec96a7335bdf37118aa9f93edca0b6a5cba2d1
anpandu <anpandumail@gmail.com>
2014-11-20 13:36:54 +0700

    Info5W1H: unit test

--- module/Info5W1H.py
+++ module/Info5W1H.py
@@ -1,16 +1,14 @@
-
 from tokenizer import Tokenizer
 
 class Info5W1H(object):
 
 	def __init__(self, _what, _who, _when, _where, _why, _how, _text):
-		self.tokenizer = Tokenizer()
-		self.what = self.tokenizer.removeNonAscii(_what)
-		self.who = self.tokenizer.removeNonAscii(_who)
-		self.when = self.tokenizer.removeNonAscii(_when)
-		self.where = self.tokenizer.removeNonAscii(_where)
-		self.why = self.tokenizer.removeNonAscii(_why)
-		self.how = self.tokenizer.removeNonAscii(_how)
-		self.text = self.tokenizer.removeNonAscii(_text)
-		self.sentences = self.tokenizer.getSentences(self.text)
-		self.tokenized_sentences = [self.tokenizer.getTokens(sentence) for sentence in self.sentences]
+		self.what = Tokenizer.removeNonAscii(_what)
+		self.who = Tokenizer.removeNonAscii(_who)
+		self.when = Tokenizer.removeNonAscii(_when)
+		self.where = Tokenizer.removeNonAscii(_where)
+		self.why = Tokenizer.removeNonAscii(_why)
+		self.how = Tokenizer.removeNonAscii(_how)
+		self.text = Tokenizer.removeNonAscii(_text)
+		self.sentences = Tokenizer.getSentences(self.text)
+		self.tokenized_sentences = [Tokenizer.getTokens(sentence) for sentence in self.sentences]
---
+++ tests/TestInfo5W1H.py
@@ -0,0 +1,18 @@
+from module.Info5W1H import Info5W1H
+
+class TestInfo5W1H:
+
+    @classmethod
+    def setup_class(self):
+        self.text = "Mama, just killed a man, Put a gun against his head, Pulled my trigger, now he's dead. Mama, life had just begun, but now I've gone and thrown it all away."
+        self.what = ""
+        self.who = ""
+        self.when = ""
+        self.where = ""
+        self.why = ""
+        self.how = ""
+
+    def test_init(self):
+    	assert info.text == "Mama, just killed a man, Put a gun against his head, Pulled my trigger, now he's dead. Mama, life had just begun, but now I've gone and thrown it all away."
+    	assert info.sentences == ["Mama, just killed a man, Put a gun against his head, Pulled my trigger, now he's dead", "Mama, life had just begun, but now I've gone and thrown it all away"]
+    	assert info.tokenized_sentences == [["Mama", ",", "just", "killed", "a", "man", ",", "Put", "a", "gun", "against", "his", "head", ",", "Pulled", "my", "trigger", ",", "now", "he's", "dead"], ["Mama", ",", "life", "had", "just", "begun", ",", "but", "now", "I've", "gone", "and", "thrown", "it", "all", "away"]]
