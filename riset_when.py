136f3170e652a3276b0918405434a84d927c4f59
anpandu <anpandumail@gmail.com>
2014-11-30 15:20:35 +0700

    riset when

--- riset_when.py
+++ riset_when.py
@@ -8,6 +8,7 @@ if len(argv)>1:
 from module.classifier import Classifier
 from module.dataprovider import MDP
 from module.featureextractor import FeatureExtractor
+from module.fe.fe_when import FeatureExtractorWhen
 from module.tokenizer import Tokenizer
 import nltk
 
@@ -19,7 +20,13 @@ for x in infos:
 	twhen = Tokenizer.getTokens(x.when)
 	prevwhen = FeatureExtractor.prevToken(x.when, x.text)
 	nextwhen = FeatureExtractor.nextToken(x.when, x.text)
-	res = {'twhen':twhen, 'prevwhen':prevwhen, 'nextwhen':nextwhen}
+	ntoken = FeatureExtractor.countToken(when)
+	day = FeatureExtractorWhen.isContainingDayNames(when)
+	date = FeatureExtractorWhen.isContainingDateNumbers(when)
+	month = FeatureExtractorWhen.isContainingMonthNames(when)
+	year = FeatureExtractorWhen.isContainingYear(when)
+	slash = FeatureExtractor.isIncludingString("/",when)
+	res = {'twhen':twhen, 'prevwhen':prevwhen, 'nextwhen':nextwhen, 'ntoken':ntoken, 'day':day, 'date':date, 'month':month, 'year':year, 'slash':slash}
 	results.append(res)
 
 if (arg=="json"):
@@ -27,15 +34,47 @@ if (arg=="json"):
 	print json.dumps(results, indent=2)
 elif(arg=="log"):
 	## LOG
-	print "=== twhen ==="
+	print "=== token ==="
 	for x in results:
 		print x["twhen"]
 	print ""
-	print "=== prevwhen ==="
+	
+	print "=== jumlah token ==="
+	for x in results:
+		print x["ntoken"]
+	print ""
+	
+	print "=== mengandung nama hari ==="
+	for x in results:
+		print x["day"]
+	print ""
+	
+	print "=== mengandung angka 1-31 (tanggal) ==="
+	for x in results:
+		print x["date"]
+	print ""
+	
+	print "=== mengandung nama bulan ==="
+	for x in results:
+		print x["month"]
+	print ""
+	
+	print "=== mengandung nama tahun ==="
+	for x in results:
+		print x["year"]
+	print ""
+
+	print "=== mengandung '/' ==="
+	for x in results:
+		print x["slash"]
+	print ""
+
+	print "=== prev token ==="
 	for x in results:
 		print x["prevwhen"]
 	print ""
-	print "=== nextwhen ==="
+
+	print "=== next token ==="
 	for x in results:
 		print x["nextwhen"]
 	print ""
