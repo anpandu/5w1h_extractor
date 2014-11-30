2b06ecf10078ba5bf9eb975ed7ecd53da8da358d
anpandu <anpandumail@gmail.com>
2014-11-30 16:39:55 +0700

    Classifier: save & load classifier

--- module/classifier.py
+++ module/classifier.py
@@ -1,4 +1,5 @@
 import nltk
+import pickle
 from module.tokenizer import Tokenizer
 from module.fe.fe_when import FeatureExtractorWhen
 
@@ -12,3 +13,15 @@ class Classifier(object):
 				fiturs.append( (FeatureExtractorWhen.getFeatureWhen(token, info.text), Tokenizer.getTokens(token)==Tokenizer.getTokens(info.when)) )
 		c = nltk.NaiveBayesClassifier.train(fiturs)
 		return {"classifier": c, "fiturs": fiturs}
+
+	@staticmethod
+	def saveClassifier(c, filename):
+		cfile = open("%s.classifier" % (filename), "w")
+		pickle.dump(c, cfile)
+		cfile.close
+
+	@staticmethod
+	def loadClassifier(filename):
+		cfile = open("%s.classifier" % (filename), "r")
+		c = pickle.load(cfile)
+		return c
--- tests/TestClassifier.py
+++ tests/TestClassifier.py
@@ -1,4 +1,5 @@
 import nltk
+import os
 from module.classifier import Classifier
 from module.dataprovider import MDP
 from module.featureextractor import FeatureExtractor
@@ -13,3 +14,11 @@ class TestClassifier:
         temp = Classifier.trainWhen(MDP.get5w1h([6]))
         assert "classifier" in temp
         assert "fiturs" in temp
+
+    def test_saveClassifier_loadClassifier(self):
+        temp = Classifier.trainWhen(MDP.get5w1h([6]))
+        c = temp["classifier"]
+        Classifier.saveClassifier(c, "whentest")
+        c2 = Classifier.loadClassifier("whentest")
+        assert isinstance(c2, nltk.classify.naivebayes.NaiveBayesClassifier)
+        os.remove("whentest.classifier")
