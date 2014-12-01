ad44ce4976cef627c15c56a142507041c544e842
anpandu <anpandumail@gmail.com>
2014-12-01 19:06:00 +0700

    Web Service: API When return

--- module/webservice/api/apiwhen.py
+++ module/webservice/api/apiwhen.py
@@ -5,5 +5,5 @@ class ApiWhen(object):
 	@staticmethod
 	def getWhen(text):
 	    ctokens = Classifier.getClassifiedTokens("when", text)
-	    ctokenstrue = [ctoken for ctoken in ctokens if ctoken[0]==True]
+	    ctokenstrue = [ctoken[1] for ctoken in ctokens if ctoken[0]==True]
 	    return ctokenstrue
