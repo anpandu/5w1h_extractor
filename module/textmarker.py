ccde35263678bcc479af9416571678d28f36a764
anpandu <anpandumail@gmail.com>
2014-12-02 18:15:37 +0700

    TextMarker: v1.0, tunggu approval pembimbing

--- module/textmarker.py
+++ module/textmarker.py
@@ -1,9 +1,11 @@
 from dataprovider import MDP
+from tokenizer import Tokenizer
 
 class TextMarker(object):
 
-	bmarks = ['_bwhat_', '_bwho_', '_bwhen_', '_bwhere_', '_bwhy_', '_bhow_']
-	emarks = ['_ewhat_', '_ewho_', '_ewhen_', '_ewhere_', '_ewhy_', '_ehow_']
+	btags = ['[bwhat]', '[bwho]', '[bwhen]', '[bwhere]', '[bwhy]', '[bhow]']
+	etags = ['[ewhat]', '[ewho]', '[ewhen]', '[ewhere]', '[ewhy]', '[ehow]']
+	othertags = ['[bother]', '[eother]']
 
 	@staticmethod
 	def getMarkedTexts(infos):
@@ -17,8 +19,39 @@ class TextMarker(object):
 			if (getattr(info, propname)!='-' and getattr(info, propname)!=''):
 				begin = text.find(getattr(info, propname))
 				if (begin>-1):
-					text = text[:begin] + TextMarker.bmarks[i] + text[begin:]
+					text = text[:begin] + TextMarker.btags[i] + text[begin:]
 					begin = text.find(getattr(info, propname))
 					end = begin+len(getattr(info, propname))
-					text = text[:end] + TextMarker.emarks[i] + text[end:]
+					text = text[:end] + TextMarker.etags[i] + text[end:]
+		return text
+
+	@staticmethod
+	def getOtherMarkedText(markedtext):
+		btags2 = ['B_WHAT', 'B_WHO', 'B_WHEN', 'B_WHERE', 'B_WHY', 'B_HOW']
+		etags2 = ['E_WHAT', 'E_WHO', 'E_WHEN', 'E_WHERE', 'E_WHY', 'E_HOW']
+
+		for i, tag in enumerate(btags2):
+			markedtext = markedtext.replace(TextMarker.btags[i], tag)
+		for i, tag in enumerate(etags2):
+			markedtext = markedtext.replace(TextMarker.etags[i], tag)	
+
+		text = ""
+		state = 0
+		for token in Tokenizer.getTokens(markedtext):
+			if (reduce( (lambda x, y: x or y), list(map((lambda x: x in token), btags2)) )):
+				state += 1
+			if (state==0):
+				# print "%s\t%s" % (state, TextMarker.othertags[0] + token + TextMarker.othertags[1])
+				text += TextMarker.othertags[0] + token + TextMarker.othertags[1]
+			else:
+				# print "%s\t%s" % (state, token)
+				text += token + " "
+			if (reduce( (lambda x, y: x or y), list(map((lambda x: x in token), etags2)) )):
+				state -= 1
+
+		for i, tag in enumerate(TextMarker.btags):
+			text = text.replace(btags2[i], tag)
+		for i, tag in enumerate(TextMarker.etags):
+			text = text.replace(etags2[i], tag)	
+
 		return text
--- tests/TestTextMarker.py
+++ tests/TestTextMarker.py
@@ -1,23 +1,44 @@
 from module.dataprovider import MDP
 from module.textmarker import TextMarker
+import re
+import json
 
 class TestTextMarker:
 
 	@classmethod
 	def setup_class(self):
-		self.paragraph = MDP.get5w1h([6])[1]
+		self.info = MDP.get5w1h([6])[1]
 
 	def test_getMarkedText(self):
-		text = self.paragraph
-		res = TextMarker.getMarkedText(text)
-		assert "_bwhat_" in res
-		assert "_bwho_" in res
-		assert "_bwhen_" in res
-		assert "_bwhere_" in res
-		assert "_bwhy_" in res
-		assert "_bhow_" in res
+		info = self.info
+		# print json.dumps(info, default=lambda o: o.__dict__, indent=2)
+		# assert 1==2
+		res = TextMarker.getMarkedText(info)
+		assert "[bwhat]" in res
+		assert "[bwho]" in res
+		assert "[bwhen]" in res
+		assert "[bwhere]" in res
+		assert "[bwhy]" in res
+		assert "[bhow]" in res
 
 	def test_getMarkedTexts(self):
-		texts = MDP.get5w1h([6])
-		res = TextMarker.getMarkedTexts(texts)
+		infos = MDP.get5w1h([6])
+		res = TextMarker.getMarkedTexts(infos)
 		assert len(res)>1
+
+	def test_getOtherMarkedText(self):
+		info = self.info
+		res = TextMarker.getMarkedText(info)
+		res2 = TextMarker.getOtherMarkedText(res)
+		print res
+		print ''
+		searchObj = re.findall( r'(\[b.+?\].+?\[e.+?\])', res2)
+		for x in searchObj:
+			print x
+		assert re.search(r'(\[bwhat\].+?\[ewhat\])', res2)
+		assert re.search(r'(\[bwho\].+?\[ewho\])', res2)
+		assert re.search(r'(\[bwhen\].+?\[ewhen\])', res2)
+		assert re.search(r'(\[bwhere\].+?\[ewhere\])', res2)
+		assert re.search(r'(\[bwhy\].+?\[ewhy\])', res2)
+		assert re.search(r'(\[bhow\].+?\[ehow\])', res2)
+		assert re.search(r'(\[bother\].+?\[eother\])', res2)
