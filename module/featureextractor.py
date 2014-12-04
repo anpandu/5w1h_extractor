6776c902cbd818d6e13efd00e152e4404cdab46b
anpandu <anpandumail@gmail.com>
2014-12-04 23:52:17 +0700

    FeatureExtractor: runCommand

--- module/featureextractor.py
+++ module/featureextractor.py
@@ -1,5 +1,6 @@
 from tokenizer import Tokenizer
 from fe.fe_when import FeatureExtractorWhen
+import subprocess
 
 class FeatureExtractor(object):
 
@@ -7,3 +8,8 @@ class FeatureExtractor(object):
 	def getFeature(word, text):
 		fwhen = FeatureExtractorWhen.getFeature(word, "")
 		return fwhen
+
+	@staticmethod
+	def runCommand(command):
+		p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		return p.stdout.readlines()
--- tests/TestFeatureExtractor.py
+++ tests/TestFeatureExtractor.py
@@ -9,3 +9,8 @@ class TestFeatureExtractor:
     def test_getFeature(self):
     	print FeatureExtractor.getFeature("19 Oktober 2014", "")
     	assert 1==1
+
+	def test_runCommand(self):
+		command = "echo hahaha"
+		results = FeatureExtractor.runCommand(command)
+		assert results[0]=="hahaha\n"
