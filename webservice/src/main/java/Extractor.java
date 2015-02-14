d9c2f688a4356f5c2a3fe62c4ad4d59480ed3280
anpandu <anpandumail@gmail.com>
2015-02-14 19:09:52 +0700

    Webservice: getFeatures minus classification

--- webservice/src/main/java/Extractor.java
+++ webservice/src/main/java/Extractor.java
@@ -17,6 +17,7 @@ public class Extractor {
         String sentence = StringUtils.join(Tokenizer.getTokens(_news), " ");
         System.out.println(sentence);
         LinkedList<Feature> fs = Extractor.getFeatures(sentence);
+        for (Feature f : fs) {System.out.println(f.toCSVString());}
 
         res.put("news", _news);
         res.put("what", "");
@@ -31,19 +32,19 @@ public class Extractor {
     }
 
     public static LinkedList<Feature> getFeatures (String _sentence) {
-        LinkedList<Feature> res = new LinkedList();
+        LinkedList<Feature> bef0features = new LinkedList();
+        LinkedList<Feature> bef1features = new LinkedList();
+        LinkedList<Feature> bef2features = new LinkedList();
         IndonesianNETagger inner = new IndonesianNETagger();
         String sentence = _sentence;
         inner.setSentence(sentence);
         inner.extractNamedEntity(sentence);
-
         ArrayList<String> tokens = inner.getToken();
         ArrayList<String> tokkinds = inner.getTokenKind();
         ArrayList<String> nes = inner.getNE();
         ArrayList<String> cfs = inner.getContextualFeature();
         ArrayList<String> mfs = inner.getMorphologicalFeature();
         ArrayList<String> pfs = inner.getPOSFeature();
-
         for (int i = 0; i < tokens.size(); i++) {
             String[] item = new String[6];
             item[0] = tokens.get(i);
@@ -52,10 +53,29 @@ public class Extractor {
             item[3] = cfs.get(i);
             item[4] = mfs.get(i);
             item[5] = pfs.get(i);
-            Feature fff = new Feature(item[0], item[1], item[2], item[3], item[4], item[5], "1");
-            res.add(fff);
-            // System.out.println(fff.toString());
+            Feature fff = new Feature(item[0], item[1], item[2], item[3], item[4], item[5], "1", "BEGIN");
+            bef0features.add(fff);
         }
+        Feature beginFeature = new Feature("?", "BEGIN", "BEGIN", "?", "?", "?", "0", "BEGIN");
+        for (int i = 0; i < bef0features.size(); i++) {
+            Feature feature = new Feature(bef0features.get(i));
+            if (i<1)
+                feature.setBef1(beginFeature);
+            else
+                feature.setBef1(bef1features.get(i-1));
+            // set class/label here!
+            bef1features.add(feature);
+        }
+        for (int i = 0; i < bef0features.size(); i++) {
+            Feature feature = new Feature(bef1features.get(i));
+            if (i<2)
+                feature.setBef2(beginFeature);
+            else
+                feature.setBef2(bef2features.get(i-2));
+            // set class/label here!
+            bef2features.add(feature);
+        }
+        LinkedList<Feature> res = new LinkedList(bef2features);
         return res;
     }
 
--- webservice/src/main/java/Feature.java
+++ webservice/src/main/java/Feature.java
@@ -10,8 +10,11 @@ public class Feature {
     private String mf;
     private String pf;
     private String idxsentence;
+    private String label;
+    private Feature bef1;
+    private Feature bef2;
 
-    public Feature(String token, String tokkind, String ne, String cf, String mf, String pf, String idxsentence) {
+    public Feature(String token, String tokkind, String ne, String cf, String mf, String pf, String idxsentence, String label) {
         this.token = token;
         this.tokkind = tokkind;
         this.ne = ne;
@@ -19,74 +22,71 @@ public class Feature {
         this.mf = mf;
         this.pf = pf;
         this.idxsentence = idxsentence;
+        this.label = label;
     }
 
-    public String getToken() {
-        return token;
+    public Feature(Feature _f) {
+        this.token = _f.token;
+        this.tokkind =_f. tokkind;
+        this.ne = _f.ne;
+        this.cf = _f.cf;
+        this.mf = _f.mf;
+        this.pf = _f.pf;
+        this.idxsentence = _f.idxsentence;
+        this.label = _f.label;
+        if (_f.getBef1()!=null) this.setBef1(_f.getBef1());
+        if (_f.getBef2()!=null) this.setBef1(_f.getBef2());
     }
 
-    public void setToken(String token) {
-        this.token = token;
+
+
+    public String getToken() {return token;}
+    public String getTokkind() {return tokkind;}
+    public String getNe() {return ne;}
+    public String getCf() {return cf;}
+    public String getMf() {return mf;}
+    public String getPf() {return pf;}
+    public String getIdxsentence() {return idxsentence;}
+    public String getLabel() {return label;}
+    public Feature getBef1() {return bef1;}
+    public Feature getBef2() {return bef2;}
+
+    public void setToken(String token) {this.token = token;}
+    public void setTokkind(String tokkind) {this.tokkind = tokkind;}
+    public void setNe(String ne) {this.ne = ne;}
+    public void setCf(String cf) {this.cf = cf;}
+    public void setMf(String mf) {this.mf = mf;}
+    public void setPf(String pf) {this.pf = pf;}
+    public void setIdxsentence(String idxsentence) {this.idxsentence = idxsentence;}
+    public void setLabel(String label) {this.label = label;}
+    public void setBef1(Feature bef1) {this.bef1 = new Feature(bef1);}
+    public void setBef2(Feature bef2) {this.bef2 = new Feature(bef2);}
+
+    public String toCSVString0() {
+        String res = "" +
+                "'" + token + "'," +
+                "'" + tokkind + "'," +
+                "'" + ne + "'," +
+                "'" + cf + "'," +
+                "'" + mf + "'," +
+                "'" + pf + "'," +
+                "'" + idxsentence + "'," +
+                "'" + label + "'";
+        return res;
     }
 
-    public String getTokkind() {
-        return tokkind;
-    }
-
-    public void setTokkind(String tokkind) {
-        this.tokkind = tokkind;
-    }
-
-    public String getNe() {
-        return ne;
-    }
-
-    public void setNe(String ne) {
-        this.ne = ne;
-    }
-
-    public String getCf() {
-        return cf;
-    }
-
-    public void setCf(String cf) {
-        this.cf = cf;
-    }
-
-    public String getMf() {
-        return mf;
-    }
-
-    public void setMf(String mf) {
-        this.mf = mf;
-    }
-
-    public String getPf() {
-        return pf;
-    }
-
-    public void setPf(String pf) {
-        this.pf = pf;
-    }
-
-    public String getIdxsentence() {
-        return idxsentence;
-    }
-
-    public void setIdxsentence(String idxsentence) {
-        this.idxsentence = idxsentence;
-    }
-
-    @Override
-    public String toString() {
-        return "Feature{" +
-                "token='" + token + '\'' +
-                ", tokkind='" + tokkind + '\'' +
-                ", ne='" + ne + '\'' +
-                ", cf='" + cf + '\'' +
-                ", mf='" + mf + '\'' +
-                ", pf='" + pf + '\'' +
-                ", idxsentence='" + idxsentence + '\'' +
-                '}';
+    public String toCSVString() {
+        String res = "" +
+                "'" + token + "'," +
+                "'" + tokkind + "'," +
+                "'" + ne + "'," +
+                "'" + cf + "'," +
+                "'" + mf + "'," +
+                "'" + pf + "'," +
+                "'" + idxsentence + "'," +
+                "'" + label + "'";
+        if (bef1 != null) res = res + "\t|\t" + bef1.toCSVString0();
+        if (bef2 != null) res = res + "\t|\t" + bef2.toCSVString0();
+        return res;
     }
 }
